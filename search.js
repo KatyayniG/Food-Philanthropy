13ff2c12b8f94b86df907cc82c54db7cfa6b740e
Katyayni Gupta <kgupta4@illinois.edu>
2016-04-27 02:08:46 -0500

    added user functionality

--- search.js
+++ search.js
@@ -96,18 +96,18 @@ function test() {
     });
 };
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    alert("Geolocation is not supported by this browser.");
-  }
-}
-function showPosition(position) {
-  var lat = position.coords.latitude;
-  var lng = position.coords.longitude;
-  map.setCenter(new google.maps.LatLng(lat, lng));
-}
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else {
+//     alert("Geolocation is not supported by this browser.");
+//   }
+// }
+// function showPosition(position) {
+//   var lat = position.coords.latitude;
+//   var lng = position.coords.longitude;
+//   map.setCenter(new google.maps.LatLng(lat, lng));
+// }
 
 
            
